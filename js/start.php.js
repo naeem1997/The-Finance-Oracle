9fabc5afd11ebc8e3763e1b5275102aaa5b7646d
Naeem Shaikh <naeem1997@gmail.com>
2016-04-16 16:48:36 -0500

    Added everything and changed everything

---
+++ js/start.php.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+document.write('<div id="smartchatbox_img745769724" style="width: 100%; height: 450px; overflow: hidden; margin: auto; padding: 0; border: 1px solid #187cc4; ">');
+document.write('<div id="smartchatbox745769724" style="width: 100%; height: 450px; overflow: hidden; margin: auto; padding: 0; border:0; ">');
+
+
+document.write('<iframe src="http://www4.smartchatbox.com/shoutbox/sb.php?key=745769724" scrolling="no" frameborder="0" width="100%" height="450px" style="border:0; margin:0; padding: 0;">');
+document.write('</iframe></div></div>');
